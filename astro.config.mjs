6225c36e346db5284a8c894530e66df116b283ec
exco-consulting <78353355+exco-consulting@users.noreply.github.com>
2023-03-14 14:39:14 +0100

    Added flowbite TailwindCSS Admin components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import LogRocket from 'logrocket';
+LogRocket.init('kmpdi9/demo');
 
 // https://astro.build/config
 export default defineConfig({
